35a025e095977b3b729a39038f2b9636e4fae88b
prasanna1504 <prasannad150904@gmail.com>
2026-01-06 21:45:46 +0530

    fix: add missing imports for os and dj_database_url

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e-k_vkcq*ptb-19li_@qg*_nz=orcl@1c(=g=(h+5i-&(m52hk'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-e-k_vkcq*ptb-19li_@qg*_nz=orcl@1c(=g=(h+5i-&(m52hk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
