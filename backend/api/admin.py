b280b631ba962aabeabbc4d5d9e298152d940ca2
prasanna1504 <prasannad150904@gmail.com>
2026-01-07 15:35:55 +0530

    feat: enable json editing in admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,4 +4,4 @@ from .models import Menu
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('month_name', 'created_at')
-    readonly_fields = ('json_data', 'created_at')
+    readonly_fields = ('created_at',)
